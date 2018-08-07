ec2bab78a6fb7ea68c81e1ddae1f45e7ac3a249f
Roman Klein <web@romanklein.com>
2018-08-07 12:03:26 +0200

    2.0.0 Release Candidate 2

--- events/event.multilingual_redirect.php
+++ events/event.multilingual_redirect.php
@@ -32,7 +32,7 @@ class eventmultilingual_redirect extends Event
                 'website' => 'https://github.com/twiro/multilingual/'
             ),
 	        'version'      => '2.0.0',
-	        'release-date' => '2018-08-04',
+	        'release-date' => '2018-08-07',
 	        'trigger-condition' => ''
         );
     }
